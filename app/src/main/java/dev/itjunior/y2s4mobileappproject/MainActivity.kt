a77b2a25b8d00364cf2fa74d90374bd194f76682
Itamar Junior <itjunior@gmail.com>
2022-03-06 20:54:58 +0000

    buttom

--- app/src/main/java/dev/itjunior/y2s4mobileappproject/MainActivity.kt
+++ app/src/main/java/dev/itjunior/y2s4mobileappproject/MainActivity.kt
@@ -1,11 +1,21 @@
 package dev.itjunior.y2s4mobileappproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val edButton = findViewById<Button>(R.id.button)
+        edButton.setOnClickListener {
+            val intent = Intent (this, Skills ::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
